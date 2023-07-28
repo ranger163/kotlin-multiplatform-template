5941c84c0b24b6f22cd0fc764e2e0206ba352bfa
ranger163 <ahmed.nassar163@gmail.com>
2023-07-28 16:48:35 +0300

    Implementing kamel image loader

---
+++ shared/src/commonMain/kotlin/common/component/Composables.kt
@@ -0,0 +1,62 @@
+package common.component
+
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.BrokenImage
+import androidx.compose.material3.LinearProgressIndicator
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.unit.dp
+import io.kamel.image.KamelImage
+import io.kamel.image.asyncPainterResource
+
+/**
+ * Created by Ahmed Nassar on 7/28/23.
+ */
+
+@Composable
+fun LoadImage(
+    modifier: Modifier = Modifier,
+    contentScale: ContentScale = ContentScale.Fit,
+    imagePath: String,
+    contentDescription: String?
+) {
+    KamelImage(
+        modifier = modifier,
+        contentScale = contentScale,
+        resource = asyncPainterResource(imagePath),
+        contentDescription = contentDescription,
+        animationSpec = tween(),
+        onLoading = { loadingProgress ->
+            Box(
+                modifier = modifier,
+                contentAlignment = Alignment.BottomCenter
+            ) {
+                LinearProgressIndicator(
+                    modifier = Modifier.fillMaxWidth().height(1.dp),
+                    progress = loadingProgress
+                )
+            }
+        },
+        onFailure = { exception ->
+            Box(
+                modifier = modifier,
+                contentAlignment = Alignment.Center
+            ) {
+                Image(
+                    modifier = Modifier.size(48.dp),
+                    imageVector = Icons.Default.BrokenImage,
+                    contentDescription = "Default image placeholder with error $exception",
+                    alpha = 0.3f
+                )
+            }
+        }
+    )
+}
