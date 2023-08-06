a823e97331480eb4407db6c1f7557aa782d7497a
ranger163 <ahmed.nassar163@gmail.com>
2023-08-06 16:54:43 +0300

    Implementing voyager navigation for in the main Scaffold.

--- shared/src/commonMain/kotlin/me/inassar/App.kt
+++ shared/src/commonMain/kotlin/me/inassar/App.kt
@@ -3,13 +3,25 @@ package me.inassar
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Scaffold
 import androidx.compose.runtime.Composable
-import me.inassar.features.feature.presentation.ui.ProductsScreen
+import cafe.adriel.voyager.navigator.CurrentScreen
+import cafe.adriel.voyager.navigator.Navigator
+import me.inassar.common.component.TopBar
+import me.inassar.features.feature.presentation.ui.products.ProductsScreen
 
 @Composable
 fun App() {
     MaterialTheme {
-        Scaffold {
-            ProductsScreen()
+        Navigator(
+            screen = ProductsScreen
+        ) { navigator ->
+            Scaffold(topBar = {
+                TopBar(
+                    title = navigator.lastItem.key,
+                    canPop = navigator.canPop,
+                    onNavigationBackClick = { navigator.pop() })
+            }) {
+                CurrentScreen()
+            }
         }
     }
 }
