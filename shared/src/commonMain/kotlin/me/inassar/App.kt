8494d71474d9da39f1c6b087d719c09976361c6f
ranger163 <ahmed.nassar163@gmail.com>
2023-08-09 20:40:33 +0300

    Implementing native platform screen transition.

--- shared/src/commonMain/kotlin/me/inassar/App.kt
+++ shared/src/commonMain/kotlin/me/inassar/App.kt
@@ -1,13 +1,19 @@
 package me.inassar
 
+import androidx.compose.animation.ExperimentalAnimationApi
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Scaffold
 import androidx.compose.runtime.Composable
-import cafe.adriel.voyager.navigator.CurrentScreen
 import cafe.adriel.voyager.navigator.Navigator
+import cafe.adriel.voyager.transitions.FadeTransition
+import cafe.adriel.voyager.transitions.ScaleTransition
+import cafe.adriel.voyager.transitions.SlideTransition
 import me.inassar.common.composable.TopBar
+import me.inassar.common.config.Platform
 import me.inassar.features.feature.presentation.ui.products.ProductsScreen
+import me.inassar.platform.currentPlatform
 
+@OptIn(ExperimentalAnimationApi::class)
 @Composable
 fun App() {
     MaterialTheme {
@@ -20,7 +26,11 @@ fun App() {
                     canPop = navigator.canPop,
                     onNavigationBackClick = { navigator.pop() })
             }) {
-                CurrentScreen()
+                when (currentPlatform) {
+                    Platform.IOS -> SlideTransition(navigator)
+                    Platform.ANDROID -> FadeTransition(navigator)
+                    Platform.DESKTOP -> ScaleTransition(navigator)
+                }
             }
         }
     }
