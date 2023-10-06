4a2a575c8d1e210408b6b7da1f275b193908a2df
Ahmed Nassar <ahmed.nassar163@gmail.com>
2023-10-07 00:01:06 +0300

    enhancement/migrate_from_moko_viewModel_to_voyager_screenModel (#20)
    - Migrate to voyager screenModel from moko viewModel.
    - Remove moko dependencies.

--- shared/src/commonMain/kotlin/me/inassar/features/feature/presentation/ui/products/FeatureScreen.kt
+++ shared/src/commonMain/kotlin/me/inassar/features/feature/presentation/ui/products/FeatureScreen.kt
@@ -8,43 +8,41 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import cafe.adriel.voyager.core.lifecycle.LifecycleEffect
+import cafe.adriel.voyager.core.model.rememberScreenModel
 import cafe.adriel.voyager.core.screen.Screen
 import cafe.adriel.voyager.core.screen.ScreenKey
 import cafe.adriel.voyager.navigator.LocalNavigator
 import cafe.adriel.voyager.navigator.currentOrThrow
-import dev.icerock.moko.mvvm.compose.getViewModel
-import dev.icerock.moko.mvvm.compose.viewModelFactory
 import me.inassar.common.composable.RenderError
 import me.inassar.common.composable.RenderLoading
 import me.inassar.features.feature.presentation.data.FeatureStrings
 import me.inassar.features.feature.presentation.data.UiProducts
 import me.inassar.features.feature.presentation.manipulator.FeatureEvents
+import me.inassar.features.feature.presentation.manipulator.FeatureModel
 import me.inassar.features.feature.presentation.manipulator.FeatureState
-import me.inassar.features.feature.presentation.manipulator.FeatureViewModel
 import me.inassar.features.feature.presentation.ui.details.ProductDetails
+import org.koin.core.component.KoinComponent
 
 /**
  * Created by Ahmed Nassar on 5/27/23.
  */
-object ProductsScreen : Screen {
+object ProductsScreen : Screen, KoinComponent {
 
     override val key: ScreenKey = FeatureStrings.PRODUCT_LIST_SCREEN_TITLE.localize()
 
     @Composable
     override fun Content() {
 
-        val viewModel: FeatureViewModel = getViewModel(key = Unit,
-            factory = viewModelFactory { FeatureViewModel() })
-        val productsState = viewModel.productsState.collectAsState().value
+        val featureModel = rememberScreenModel { FeatureModel() }
+        val productsState by featureModel.state.collectAsState()
 
-        LaunchedEffect(Unit) {
-            viewModel.onEvent(FeatureEvents.GetProducts)
-        }
+        LifecycleEffect(onStarted = { featureModel.onEvent(FeatureEvents.GetProducts) })
 
         ProductsList(productsState)
     }
